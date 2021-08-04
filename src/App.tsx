11d733d21f449c2b83bf177452cd13d9b6310118
Eldes <eldes@frux.com.br>
2021-08-04 19:58:24 -0300

    [+] Moldura base das páginas

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,27 @@ import Home from './pages/home'
 
 function App() {
 	return (
-		<BrowserRouter>
-			<Switch>
-				<Route path="/" exact component={Home} />
-				<Route path="/contato" exact component={Contato} />
-			</Switch>
-		</BrowserRouter>
+		<>
+			<header>
+				<h1>Meu site</h1>
+				<nav>
+					<a href="/">Home</a>
+					<a href="/contato">Contato</a>
+				</nav>
+			</header>
+			<main>
+				<BrowserRouter>
+					<Switch>
+						<Route path="/" exact component={Home} />
+						<Route path="/contato" exact component={Contato} />
+					</Switch>
+				</BrowserRouter>
+			</main>
+			<footer>
+				Todos os direitos reservados.
+			</footer>
+		
+		</>
 	)
 }
 
